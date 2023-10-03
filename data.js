9e776d04de55d010bdc23e9cfd8a835693d35b9b
Ramesh Pilla <ramijack4u@yahoo.com>
2023-10-03 22:37:28 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,233 @@
+var vmData = {
+    "total_results": 8,
+    "results": [
+        {
+            "tenant_uuid": "c9b42b73-0613-4e66-83f2-6fff21087488",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:32:32.976334Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.last_message_sent"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "e98c38ad-d770-4420-9552-fc9cf3ffff1c",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "c9b42b73-0613-4e66-83f2-6fff21087488",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_create",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:32:27.088808Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "create",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.tenant_uuid",
+                "uem.enrollment_user.uuid",
+                "uem.enrollment_user.audit.created_at",
+                "uem.enrollment_user.audit.created_by_admin_uuid",
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.audit.updated_by_admin_uuid",
+                "uem.enrollment_user.custom_attributes",
+                "uem.enrollment_user.managed_organization_group_uuids",
+                "uem.enrollment_user.organization_group_uuid",
+                "uem.enrollment_user.parent_organization_group_uuids[0]",
+                "uem.enrollment_user.source_type",
+                "uem.enrollment_user.user_name",
+                "uem.enrollment_user.cost_center",
+                "uem.enrollment_user.department",
+                "uem.enrollment_user.display_name",
+                "uem.enrollment_user.distinguished_name",
+                "uem.enrollment_user.domain",
+                "uem.enrollment_user.email_address",
+                "uem.enrollment_user.email_user_name",
+                "uem.enrollment_user.employee_identifier",
+                "uem.enrollment_user.external_source_identifier",
+                "uem.enrollment_user.first_name",
+                "uem.enrollment_user.middle_name",
+                "uem.enrollment_user.last_name",
+                "uem.enrollment_user.is_active",
+                "uem.enrollment_user.last_message_sent",
+                "uem.enrollment_user.manager_uuid",
+                "uem.enrollment_user.mobile_number",
+                "uem.enrollment_user.phone_number",
+                "uem.enrollment_user.user_group_uuids",
+                "uem.enrollment_user.user_principal_name",
+                "uem.sequence_number"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "3dd4fa79-1d81-43c2-a4a0-2b75c3148d03",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "c9b42b73-0613-4e66-83f2-6fff21087488",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:32:27.07094Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.custom_attributes"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "5ea58932-5d27-422b-827a-bc89d8eeb12e",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "c9b42b73-0613-4e66-83f2-6fff21087488",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:32:27.02424Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.managed_organization_group_uuids"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "969cf2bf-fa2d-4428-a950-6ad253fa269d",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "3e864851-009c-4e4d-b416-57a654c320f4",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_delete",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:31:12.79742Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "delete",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.tenant_uuid",
+                "uem.enrollment_user_uuid",
+                "uem.sequence_number"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "ff84e422-9001-4dae-b8c7-99101bd7713f",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "91077e26-22d9-4639-be6c-9d6eab640038",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_delete",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T10:30:37.387052Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "delete",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.tenant_uuid",
+                "uem.enrollment_user_uuid",
+                "uem.sequence_number"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "73813200-7474-4b80-9d31-6ed1a387199e",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "8b1c6628-9020-40f3-839c-334d6db40b37",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T09:57:35.601291Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.display_name",
+                "uem.enrollment_user.first_name"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "ea91c2dd-536f-497b-b08e-df6412b45245",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "8b1c6628-9020-40f3-839c-334d6db40b37",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.enrollment_user",
+            "message_timestamp": "2023-10-01T09:57:35.60129Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.display_name",
+                "uem.enrollment_user.first_name",
+                "uem.enrollment_user.last_name"
+            ],
+            "source": "uem.console-replication.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "643109be-3b7c-449e-ab84-7ef86054c879",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "8b1c6628-9020-40f3-839c-334d6db40b37",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.device",
+            "message_timestamp": "2023-10-01T09:57:35.60129Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.display_name",
+                "uem.enrollment_user.first_name",
+                "uem.enrollment_user.last_name"
+            ],
+            "source": "uem.devive-state.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "643109be-3b7c-449e-ab84-7ef86054c879",
+            "additional_data": {}
+        },
+        {
+            "tenant_uuid": "8b1c6628-9020-40f3-839c-334d6db40b37",
+            "correlation_id": "14bbedc6-2e31-4e6e-bb7e-33c0e807ea44",
+            "payload_schema_name": "uem.enrollment_user_update",
+            "entity_schema_name": "uem.device",
+            "message_timestamp": "2023-10-01T09:57:35.60129Z",
+            "payload_format": "PROTOBUF",
+            "payload_part_index": 0,
+            "payload_part_count": 1,
+            "event_type": "update",
+            "sequence_number": 0,
+            "attributes": [
+                "uem.enrollment_user.audit.updated_at",
+                "uem.enrollment_user.display_name",
+                "uem.enrollment_user.first_name",
+                "uem.enrollment_user.last_name"
+            ],
+            "source": "uem.devive-state.state-changed.v1.ldd-dgs",
+            "serialized_uuid": "643109be-3b7c-449e-ab84-7ef86054c879",
+            "additional_data": {}
+        }
+    ]
+}
+
